0061a85a550041efb101d6a2ef2b360088a4fd0b
Void Zone <zonevoid3@gmail.com>
2025-07-09 01:34:37 +0000

    chnage font to inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,6 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body className="font-body antialiased">
         <Providers>
