aa8c3238efa7f10fbdd740a1437b541a8f20ac80
Void Zone <zonevoid3@gmail.com>
2025-07-11 09:40:16 +0000

    the month and year dropdown still not avaiable

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -20,14 +20,14 @@ function Calendar({
     <DayPicker
       captionLayout="dropdown-nav"
       fromYear={new Date().getFullYear() - 100}
-      toYear={new Date().getFullYear() + 50}
+      toYear={new Date().getFullYear() + 5}
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium hidden",
         caption_dropdowns: "flex justify-center gap-1",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
