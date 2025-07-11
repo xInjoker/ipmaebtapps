8ac9b11209d4c591d8968cb7d370ac59855b94aa
Void Zone <zonevoid3@gmail.com>
2025-07-11 07:10:40 +0000

    the menu icon on the header, make it bigger

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -271,11 +271,11 @@ const SidebarTrigger = React.forwardRef<
       data-sidebar="trigger"
       variant="ghost"
       size="icon"
-      className={cn("h-7 w-7", className)}
+      className={cn("h-9 w-9", className)}
       onClick={toggleSidebar}
       {...props}
     >
-      <Icon />
+      <Icon className="h-5 w-5" />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
