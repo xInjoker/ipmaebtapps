29ef2790fc72e1294eb7db28124671f0636e8474
Void Zone <zonevoid3@gmail.com>
2025-07-11 02:34:17 +0000

    when dark mode active, make the site header color the same as the foregr

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -9,14 +9,14 @@ import { Search } from 'lucide-react';
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-card px-4 sm:px-6 lg:px-8">
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-foreground px-4 sm:px-6 lg:px-8">
       <div className="flex items-center gap-4">
         <SidebarTrigger />
       </div>
       <div className="flex flex-1 items-center justify-end space-x-2">
         <form className="hidden sm:block">
           <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-background" />
             <Input
               type="search"
               placeholder="Search..."
