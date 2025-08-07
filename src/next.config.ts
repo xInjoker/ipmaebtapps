fa9948c9bb4f1b23d1fb3a6d66af8dbd8d55c22c
Yomi Neldi <yomi.neldi@gmail.com>
2025-08-07 07:26:37 +0000

    Runtime Error
    Error: Invalid src prop (https://firebasestorage.googleap

---
+++ src/next.config.ts
@@ -0,0 +1,35 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
